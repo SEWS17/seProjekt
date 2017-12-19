dc02d50c6badc9ca586f5b314bb88a773e84a114
conny92 <conny92@users.noreply.github.com>
2017-12-19 18:17:07 +0100

    testest

--- gestaltgesetzeSeite.php
+++ gestaltgesetzeSeite.php
@@ -34,7 +34,7 @@
 if(isset($_GET["id"]) && $idurl == 1) {
     
       
-  echo" <img src='Bilder\bild_frage3.jpg' style='height: 200px; margin-right: 0px; float:right;' alt='Design Profi Logo'/>
+  echo" <img src='Bilder\bild_frage3.jpg' style='height: 200px; margin-right: 0px; float:right;' alt='Es gibt fünf Zeilen und acht Spalten. Jede Zeile hat abwechselnd einen schwarzen Quader oder einen schwarzen Kreis. Also auf Platz eins ist ein Quader, Platz zwei besitzt einen Kreis, Platz drei wieder einen Quader und so weiter bis Platz acht. Ein Gatter aus regelmäßig angeordneten verschieden aussehenden Teilchen. Das Auge erkennt die Spalten durch die ähnlich aussehenden Elemente.'/>
      <h1 style='text-align:left;'>Gesetz der Ähnlichkeit</h1>
      
    <div style='width: 500px;'>
@@ -52,7 +52,7 @@ if(isset($_GET["id"]) && $idurl == 1) {
       
       if(isset($_GET["id"]) && $idurl == 2) {
 
-    echo "<img src='Bilder\bild_frage4.png' style='height: 200px; margin-right: 0px; float:right;' alt='Design Profi Logo'/>
+    echo "<img src='Bilder\bild_frage4.png' style='height: 200px; margin-right: 0px; float:right;' alt='Links kommen mehrere Fische wirr im Raum vor. Auf der rechten Seite formiert sich ein Fischschwarm zu einem größeren Fisch, diese Fische sind sehr nah beieinander. Somit erkennt man den Schwarm nicht als einzelne Fische, sondern als neue Form.'/>
      
     <h1 style='text-align:left;'>Gesetz der Nähe</h1>";
 
@@ -72,7 +72,7 @@ if(isset($_GET["id"]) && $idurl == 1) {
         if(isset($_GET["id"]) && $idurl == 3) {
             
               
-          echo" <img src='Bilder\bild_frage6.png' style='height: 200px; margin-right: 0px; float:right;' alt='Design Profi Logo'/>
+          echo" <img src='Bilder\bild_frage6.png' style='height: 200px; margin-right: 0px; float:right;' alt='Durch viele Kreise wird eine Welle dargestellt. Erst steigt sie langsam an, hat einen Hochpunkt und fällt wieder mit Schwung. Es entsteht eine harmonische Bewegung in die Leserichtung und sieht gut aus'/>
           <h1 style='text-align:left;'>Gesetz der guten Fortsetzung</h1>
            <div style='width: 500px;'>
            Das Gesetz der guten Fortsetzung besagt, dass Elemente, 
@@ -87,7 +87,7 @@ if(isset($_GET["id"]) && $idurl == 1) {
   if(isset($_GET["id"]) && $idurl == 4) {
                         
                           
-                      echo" <img src='Bilder\bild_frage1.jpg' style='height: 200px; margin-right: 0px; float:right;' alt='Design Profi Logo'/>
+                      echo" <img src='Bilder\bild_frage1.jpg' style='height: 200px; margin-right: 0px; float:right;' alt='Durch Aussparung und Weißraum wird ein Panda dargestellt. Mit schwarzen Kreisen die Ohren, Nase, Augen und Beine. Der Betrachter vervollständigt das Tier automatisch, obwohl es nur angedeutet wird'/>
                       <h1 style='text-align:left;'>Gesetz der Geschlossenheit</h1>
                        <div style='width: 500px;'>
                        Das Gesetz der Geschlossenheit besagt, dass Dinge mit geschlossenem Umriss
@@ -113,7 +113,7 @@ if(isset($_GET["id"]) && $idurl == 1) {
   if(isset($_GET["id"]) && $idurl == 5) {
     
       
-  echo" <img src='Bilder\bild_frage5.png' style='height: 200px; margin-right: 0px; float:right;' alt='Design Profi Logo'/>
+  echo" <img src='Bilder\bild_frage5.png' style='height: 200px; margin-right: 0px; float:right;' alt='Es werden Kreise mit Klammern dargestellt. Eine Klammer öffnet sich und lässt ein wenig Weissraum, danach schließt sie wieder. Das wiederholt sich zwei weitere Male. Der Betrachter erkennt direkt die Mittelachsen durch die Kreise und die resultierende Symmetrie der Klammern'/>
   <h1 style='text-align:left;'>Gesetz der Symmetrie</h1>
    <div style='width: 500px;'>
    Bei den Gestaltgesetzen aus der Gestaltpsychologie hat das Gesetz der Symmetrie
@@ -127,7 +127,7 @@ if(isset($_GET["id"]) && $idurl == 1) {
             if(isset($_GET["id"]) && $idurl == 6) {
                 
                   
-              echo" <img src='Bilder\bild_frage2.jpg' style='height: 200px; margin-right: 0px; float:right;' alt='Design Profi Logo'/>
+              echo" <img src='Bilder\bild_frage2.jpg' style='height: 200px; margin-right: 0px; float:right;' alt='Sieben schwarze Kreise kommen ungeordnet im Raum vor. Darüber spannt sich ein Quader in weiß auf und wird erkennbar dadurch, dass jede Ecke jeweils durch einen Kreis hinterlegt ist. Nur die Ecken werden erkenntlich gemacht und man erkennt die Figur durch zusammenführen der Seiten. Der Quader ist eine Grundform, was bei diesem Gesetz wichtig ist, jeder muss die Figur kennen.'/>
               <h1 style='text-align:left;'>Gesetz der guten Gestalt</h1>
                <div style='width: 500px;'>
                Das Gesetz der Einfachheit besagt, dass das menschliche Gehirn Formen so interpretiert, 
@@ -140,7 +140,7 @@ if(isset($_GET["id"]) && $idurl == 1) {
                         if(isset($_GET["id"]) && $idurl == 7) {
                             
                               
-                          echo" <img src='Bilder\bild_frage7.png' style='height: 200px; margin-right: 0px; float:right;' alt='Design Profi Logo'/>
+                          echo" <img src='Bilder\bild_frage7.png' style='height: 200px; margin-right: 0px; float:right;' alt='Sieben schwarze Kreise kommen ungeordnet im Raum vor. Darüber spannt sich ein Quader in weiß auf und wird erkennbar dadurch, dass jede Ecke jeweils durch einen Kreis hinterlegt ist. Nur die Ecken werden erkenntlich gemacht und man erkennt die Figur durch zusammenführen der Seiten. Der Quader ist eine Grundform, was bei diesem Gesetz wichtig ist, jeder muss die Figur kennen.'/>
                           <h1 style='text-align:left;'>Gesetz des gemeinsamen Schicksals</h1>
                            <div style='width: 500px;'>
                            Das Gesetz der Einfachheit besagt, dass das menschliche Gehirn Formen so interpretiert, 
@@ -153,7 +153,7 @@ if(isset($_GET["id"]) && $idurl == 1) {
                                     if(isset($_GET["id"]) && $idurl == 8) {
                                         
                                           
-                                      echo" <img src='Bilder\bild_frage8.jpg' style='height: 200px; margin-right: 0px; float:right;' alt='Design Profi Logo'/>
+                                      echo" <img src='Bilder\bild_frage8.jpg' style='height: 200px; margin-right: 0px; float:right;' alt='Es werden Pfeile in jeweils drei Zeilen angeordnet. Die erste Zeile an Pfeilen zeigt in die rechte Richtung. Die zweite Zeile dann nach links und die letzte dritte wieder nach rechts. So sieht es aus, als würde jede Zeile dazu verdammt sein, eine Richtung zu gehen./>
                                       <h1 style='text-align:left;'>Gesetz der Kontinuität</h1>
                                        <div style='width: 500px;'>
                                        Das Gesetz der Einfachheit besagt, dass das menschliche Gehirn Formen so interpretiert, 
@@ -165,7 +165,7 @@ if(isset($_GET["id"]) && $idurl == 1) {
                                                 if(isset($_GET["id"]) && $idurl == 9) {
                                                     
                                                       
-                                                  echo" <img src='Bilder\bild_frage9.jpg' style='height: 200px; margin-right: 0px; float:right;' alt='Design Profi Logo'/>
+                                                  echo" <img src='Bilder\bild_frage9.jpg' style='height: 200px; margin-right: 0px; float:right;' alt='Eine Linie kringelt sich durch das Bild und wird oben sowie unten angeschnitten. Sie hat also kein wahrnehmbares Ende oder Anfang.'/>
                                                   <h1 style='text-align:left;'>Kontur</h1>
                                                    <div style='width: 500px;'>
                                                    Das Gesetz der Einfachheit besagt, dass das menschliche Gehirn Formen so interpretiert, 
@@ -183,16 +183,17 @@ if(isset($_GET["id"]) && $idurl < 10) {
 
     //$idurl2--;
 
-if($idurl < 10) {
-   echo "<a class='weiter' href='gestaltgesetzeSeite.php?id=".$idurl."'>weiter</a>";
-}
-
    if($idurl > 2)
 {
 $idurl2=$idurl-2;
  
 echo "<a class='zurueck' href='gestaltgesetzeSeite.php?id=".$idurl2."'>zurück</a>";
 }
+
+if($idurl < 10) {
+   echo "<a class='weiter' href='gestaltgesetzeSeite.php?id=".$idurl."'>weiter</a>";
+}
+
 }
    
   ?>
